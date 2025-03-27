ab61ccea9b06e213dc8d56918025d803bfc3553c
acheugo <hugomartinez@gmail.com>
2025-03-26 23:24:53 -0600

    First commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,55 @@
-import logo from './logo.svg';
+import logo from './Cruise0-logo.svg';
 import './App.css';
+import { useAuth0 } from '@auth0/auth0-react';
+import LogInButton from './LogInbutton';
+import LogOutButton from './LogOutButton';
+import Profile from './Profile';
 
 function App() {
+const { user, isAuthenticated, isLoading, } =useAuth0();
+
+if (isLoading){
+  return <div>Loading ... </div>  
+}
+
+// Show error if email is not verified
+if (isAuthenticated && user && !user.email_verified) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <br />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1>To come aboard, please verify your email address.</h1> Cruise0
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <LogOutButton />
+      </header>
+    </div>
+  );
+}
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        { isAuthenticated && <Profile></Profile>}
+        <img src={logo} className="App-logo" alt="logo" />
+        {!isAuthenticated && (
+      <>
+        <p> <h1>Log in to book your travel with Cruise0.</h1>    </p>
+        <LogInButton />
+      </>
+  )      }
+
+{isAuthenticated && (
+  <>
+  <h2>
+  Where your adventure begins.
+  </h2>
+   <LogOutButton />
+  </>
+)}
+      
+       
       </header>
     </div>
   );
